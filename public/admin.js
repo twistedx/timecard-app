e4111ecf848a22a14040d1850b6f7f47acbf897b
bufu2o2 <bufu2o2@gmail.com>
2019-06-22 14:28:55 -0700

    added job creation and timecard creation relative to a job and also the views of profile jobs and timecards

--- public/admin.js
+++ public/admin.js
@@ -2,9 +2,11 @@
 //welcome message
     alert('Fuck Off');
 
+    let token;
 
 
-    // login
+
+    // login=============================================================================================
     login = () => {
         let logval = document.getElementById('log').value;
         console.log(logval);
@@ -21,24 +23,150 @@
             return r.json();
         })
         .then(r => {
+
+            ///////////////////////////////////////// once we have this hooked into react we will have to save the token into a state
+
             console.log(r);
-            sessionStorage.setItem('token', r.token);
-            console.log(`this is the call of seession storage: ${sessionStorage.getItem('token')}`);
+            token = r.token;
+            console.log(`
+            this is the token stored on the front end: 
+            ${token}`);
+
+            // sessionStorage.setItem('token', r.token);
+            // console.log(`this is the call of seession storage: ${sessionStorage.getItem('token')}`);
         })
         .catch(e => console.error('ERROR: ', e));
     }
 
 
 
-    //view profile
-    viewProfile = () => { //put the user id in the fetch url
-        fetch('/api/user/5d0c237c9923b70a70571f7e', {
+    //view profile======================================================================================
+    viewProfile = () => { 
+        // console.log(`
+        // this is the session storage of your token: 
+        // ${sessionStorage.getItem('token')}`);
+
+        console.log(`
+        this is the front end storage of your token: 
+        ${token}`);
+
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        headers['x-auth-token'] = token;
+
+        fetch('/api/user/5d0c237c9923b70a70571f7e', {             //!!!!!!!!put the user id in the fetch url!!!!!!!!!!!!!!!!!!!
             method: 'GET',
-            header: {
+            headers
+        }).then(r => r.json())
+        .then(r => {
+            let res = JSON.stringify(r[0]);
+            console.log(`this is the second then after profile view fetch 
+            ${res}`);
+            document.getElementById('viewProfile').innerHTML = res;
+        })
+        .catch(e => console.error('ERROR: ', e));
+    }
+
+
+
+    //create new job================================================================================
+    newJob = () => {
+        //formmatting form responses into payload obj
+        let f = document.getElementById('newJobForm').elements;
+        let njfObj = {};
+        for(let i = 0;i<f.length; i++){
+            if(f[i].value !== 'create'){
+                njfObj[f[i].name] = f[i].value;
+            }        
+        }
+        const newObj = JSON.stringify(njfObj);
+        console.log(newObj);
+
+
+    //setting headers
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    headers['x-auth-token'] = token;
+
+    console.log(`this is the token saved front end:
+    ${token}`);
+
+    //fetch request
+        fetch('/api/job', {
+            method: 'POST',
+            body: newObj,
+            headers
+        })
+        .then(r => {
+            console.log(r);
+            return r.json();
+        })
+        .then(r => {
+            console.log(r);
+        })
+        .catch(e => console.error('ERROR: ', e));
+    }
+
+
+    //view job=====================================================================================================
+        viewJob = () => { 
+    
+            console.log(`
+            this is the front end storage of your token: 
+            ${token}`);
+    
+            const headers = {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer' + sessionStorage.getItem('token')
             }
-        }).then(r => r.text())
+            headers['x-auth-token'] = token;
+    
+            fetch('/api/job/', {
+                method: 'GET',
+                headers
+            }).then(r => r.text())
+            .then(r => {
+                console.log(`this is the second then after jobview fetch 
+                ${r}`);
+                document.getElementById('viewJob').innerHTML = r;
+            })
+            .catch(e => console.error('ERROR: ', e));
+        }
+
+
+
+
+
+
+
+//create new Timecard==========================================================================
+    newTc = () => {
+    //get job id for the URL
+    const jobId = document.getElementById('ntc').value;
+
+    //setting headers
+    const headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    headers['x-auth-token'] = token;
+
+    console.log(`this is the token saved front end:
+    ${token}`);
+
+    //fetch request
+        fetch(`/api/timecard/${jobId}`, {
+            method: 'POST',
+            headers
+        })
+        .then(r => {
+            console.log(r);
+            return r.json();
+        })
         .then(r => {
             console.log(r);
         })
@@ -47,32 +175,37 @@
 
 
 
-    //create new job
-    newJob = () => {
-        // return alert({
-        //     name: document.getElementById('njfName').value,
-        //     jobType: document.getElementsByClassName('njfJobType').value,
-        //     role: document.getElementById('njfRole').value,
-        //     description: document.getElementById('njfDes').value
-        // });
-        
-        // alert(newJobObj);
 
-        // fetch('/api/auth', {
-        //     method: 'POST',
-        //     body: logval,
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     }
-        // })
-        // .then(r => {
-        //     console.log(r);
-        //     return r.json();
-        // })
-        // .then(r => {
-        //     console.log(r);
-        //     sessionStorage.setItem('token', r.token);
-        //     console.log(`this is the call of seession storage: ${sessionStorage.getItem('token')}`);
-        // })
-        // .catch(e => console.error('ERROR: ', e));
+
+    
+    //view timecard=====================================================================================================
+    viewTc = () => { 
+        //get job id for the URL
+        const jobId = document.getElementById('vntc').value;
+
+
+
+        console.log(`
+        this is the front end storage of your token: 
+        ${token}`);
+
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        headers['x-auth-token'] = token;
+
+        fetch(`/api/timecard/${jobId}`, {
+            method: 'GET',
+            headers
+        }).then(r => r.text())
+        .then(r => {
+            console.log(`this is the second then after jobview fetch 
+            ${r}`);
+            document.getElementById('viewTc').innerHTML = r;
+        })
+        .catch(e => console.error('ERROR: ', e));
     }
+
+
+
