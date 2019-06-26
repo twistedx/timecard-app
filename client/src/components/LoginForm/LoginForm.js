6afe232145a630bc9ef4bf22dd7a072e7e7b8268
Sean Gowing <smgowing@hotmail.com>
2019-06-25 21:40:21 -0700

    adding context for Auth and Alert

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import AuthContext from '../../'
 import './LoginForm.css';
 
-class LoginForm extends Component {
-    
-    handleLogin = e => {
+const LoginForm = () => {
+
+    const handleLogin = e => {
         e.preventDefault();
 
         const email = document.getElementById("email").value;
@@ -11,13 +12,13 @@ class LoginForm extends Component {
 
         console.log(email, password);
 
-        this.resetLoginForm();
+        resetLoginForm();
 
         alert("Clicked and form reset");
 
     }
 
-    resetLoginForm() {
+    const resetLoginForm = () => {
         const email = document.getElementById("email");
         const password = document.getElementById("password");
 
@@ -25,44 +26,42 @@ class LoginForm extends Component {
         password.value = "";
     }
 
-    render() {
+    return (
+        <div className="container">
 
-        return (
-            <div className="container">
-    
-                <div className="row">
-                    <form className="col s12 m12 l12" id="loginForm">
-    
-                        <div className="row">
-                            <div className="input-field col s12 m6 l6 offset-m3 offset-l4">
-                                <input id="email" type="email" className="validate" />
-                                <label for="email">Email</label>
-                            </div>
+            <div className="row">
+                <form className="col s12 m12 l12" id="loginForm">
+
+                    <div className="row">
+                        <div className="input-field col s12 m6 l6 offset-m3 offset-l4">
+                            <input id="email" type="email" className="validate" />
+                            <label for="email">Email</label>
                         </div>
-    
-                        <div className="row">
-                            <div className="input-field col s12 m6 l6 offset-m3 offset-l4 center">
-                                <input id="password" type="password" className="validate" />
-                                <label for="password">Password</label>
-                                <a id="ForgotPassword" href="#">Forgot My Password</a> |
+                    </div>
+
+                    <div className="row">
+                        <div className="input-field col s12 m6 l6 offset-m3 offset-l4 center">
+                            <input id="password" type="password" className="validate" />
+                            <label for="password">Password</label>
+                            <a id="ForgotPassword" href="#">Forgot My Password</a> |
                                 <a id="CreateAccount" href="#">Create account</a>
-                            </div>
                         </div>
-    
-                        <div className="row">
-                            <div className="s12 m12 l12 center">
-                                <a onClick={this.handleLogin.bind(this)} className="btn blue lighten-1" id="LoginBtn">Login</a>
-                            </div>
-                        </div>
-    
-                    </form>
-                </div>
-    
-            </div>
-        );
+                    </div>
 
-    }
+                    <div className="row">
+                        <div className="s12 m12 l12 center">
+                            <a onClick={handleLogin} className="btn blue lighten-1" id="LoginBtn">Login</a>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+
+        </div>
+    );
 
 }
+
+
 
 export default LoginForm;
