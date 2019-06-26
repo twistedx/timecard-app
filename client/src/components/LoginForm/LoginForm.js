e45c2e2b00c1e24e6b174e299d55e2a865697a07
Kyle Oliveri <KyleOliveri@Kyles-Mac-Pro.local>
2019-06-25 19:48:28 -0700

    Basic frame for login page is complete. Created a handleLogin event and applied it to the login button

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,40 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './LoginForm.css';
 
-const LoginForm = () => {
-    return (
-        <div className="container">
+class LoginForm extends Component {
+    
+    handleLogin = e => {
+        e.preventDefault();
 
-            <div className="row">
-                <form className="col s12 m12 l12">
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-                    <div className="row">
-                        <div className="input-field col s12 m6 l6 offset-m3 offset-l4">
-                            <input id="email" type="email" className="validate" />
-                            <label for="email">Email</label>
+        console.log(email, password);
+
+        this.resetLoginForm();
+
+        alert("Clicked and form reset");
+
+    }
+
+    resetLoginForm() {
+        const email = document.getElementById("email");
+        const password = document.getElementById("password");
+
+        email.value = "";
+        password.value = "";
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+    
+                <div className="row">
+                    <form className="col s12 m12 l12" id="loginForm">
+    
+                        <div className="row">
+                            <div className="input-field col s12 m6 l6 offset-m3 offset-l4">
+                                <input id="email" type="email" className="validate" />
+                                <label for="email">Email</label>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="input-field col s12 m6 l6 offset-m3 offset-l4 center">
-                            <input id="password" type="password" className="validate" />
-                            <label for="password">Password</label>
-                            <a id="ForgotPassword" href="#">Forgot my password</a> |
-                            <a id="CreateAccount" href="#"> Create account</a>
+    
+                        <div className="row">
+                            <div className="input-field col s12 m6 l6 offset-m3 offset-l4 center">
+                                <input id="password" type="password" className="validate" />
+                                <label for="password">Password</label>
+                                <a id="ForgotPassword" href="#">Forgot My Password</a> |
+                                <a id="CreateAccount" href="#">Create account</a>
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="s12 m12 l12 center">
-                            <a className="btn blue lighten-1" id="LoginBtn">Login</a>
+    
+                        <div className="row">
+                            <div className="s12 m12 l12 center">
+                                <a onClick={this.handleLogin.bind(this)} className="btn blue lighten-1" id="LoginBtn">Login</a>
+                            </div>
                         </div>
-                    </div>
-
-                </form>
+    
+                    </form>
+                </div>
+    
             </div>
+        );
 
-        </div>
-    );
+    }
+
 }
 
 export default LoginForm;
