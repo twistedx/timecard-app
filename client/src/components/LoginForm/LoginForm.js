cc8273cb1032258d1d90501b90f67e2a95358946
Sean Gowing <smgowing@hotmail.com>
2019-06-25 21:48:59 -0700

    changes to the function of the login form

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,48 +1,40 @@
-import React, { useContext } from 'react';
-import AuthContext from '../../'
+import React, { useState } from 'react';
+import AlertContext from '../../context/alert/AlertContext';
+
 import './LoginForm.css';
 
 const LoginForm = () => {
 
-    const handleLogin = e => {
-        e.preventDefault();
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+    const { email, password } = user;
 
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
+    const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value })
 
-        console.log(email, password);
-
-        resetLoginForm();
-
-        alert("Clicked and form reset");
-
-    }
-
-    const resetLoginForm = () => {
-        const email = document.getElementById("email");
-        const password = document.getElementById("password");
-
-        email.value = "";
-        password.value = "";
+    const onSubmit = e => {
+        e.preventdefault();
+        console.log('Login Submit');
     }
 
     return (
         <div className="container">
 
             <div className="row">
-                <form className="col s12 m12 l12" id="loginForm">
+                <form className="col s12 m12 l12" id="loginForm" onSubmit={onSubmit}>
 
                     <div className="row">
                         <div className="input-field col s12 m6 l6 offset-m3 offset-l4">
-                            <input id="email" type="email" className="validate" />
-                            <label for="email">Email</label>
+                            <label htmlFor="email">Email Address</label>
+                            <input type="email" name="email" value={email} onChange={onChange} />
                         </div>
                     </div>
 
                     <div className="row">
                         <div className="input-field col s12 m6 l6 offset-m3 offset-l4 center">
-                            <input id="password" type="password" className="validate" />
-                            <label for="password">Password</label>
+                            <label htmlFor="password">Password</label>
+                            <input type="password" name="password" value={password} onChange={onChange} />
                             <a id="ForgotPassword" href="#">Forgot My Password</a> |
                                 <a id="CreateAccount" href="#">Create account</a>
                         </div>
@@ -50,7 +42,7 @@ const LoginForm = () => {
 
                     <div className="row">
                         <div className="s12 m12 l12 center">
-                            <a onClick={handleLogin} className="btn blue lighten-1" id="LoginBtn">Login</a>
+                            <input type="submit" value="Login" className="btn blue lighten-1" />
                         </div>
                     </div>
 
