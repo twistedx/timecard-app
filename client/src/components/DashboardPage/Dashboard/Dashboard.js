3bafa84345582c48f83b94b869829a9099a2b522
Sean Gowing <smgowing@hotmail.com>
2019-06-30 13:11:07 -0700

    fixed authentication to push to dashboard on successful login

--- client/src/components/DashboardPage/Dashboard/Dashboard.js
+++ client/src/components/DashboardPage/Dashboard/Dashboard.js
@@ -1,56 +1,72 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Navbar from '../../Layout/Navbar/Navbar.js';
 import UserDashboardCard from '../UserDashboardCard/UserDashboardCard.js';
 import Footer from '../../Layout/Footer/Footer.js';
 import BtnCardReveal from '../../BtnList/BtnCardReveal';
+import AuthContext from '../../../context/auth/AuthContext';
+import setAuthToken from '../../../utils/setAuthToken';
+
+const Dashboard = (props) => {
+
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+    }, [])
 
 
-const Dashboard = () => {
+    if (localStorage.token) {
+        setAuthToken(localStorage.token);
+    } else {
+        props.history.push('/login');
+    }
+
+
     return (
         <div>
-            
-                <header>
-                    <Navbar title="Dashboard">
-                        
-                    </Navbar>
-                </header>
-                <main>
-                    <body>
-                        <UserDashboardCard 
-                            name="Rambo"
-                            email="Rambo@rambo.com"
-                            jobTitle="Test"    
-                        />
-                        <BtnCardReveal 
-                            title = 'TEST JOB TITLE BLAH BLAH'
-                            description = 'This is a test job description'
-                            role = 'this is a test role'
-                            type = 'this is a test job type'
-                        />
-                                                <BtnCardReveal 
-                            title = 'TEST JOB TITLE BLAH BLAH'
-                            description = 'This is a test job description'
-                            role = 'this is a test role'
-                            type = 'this is a test job type'
-                        />
-                                                <BtnCardReveal 
-                            title = 'TEST JOB TITLE BLAH BLAH'
-                            description = 'This is a test job description'
-                            role = 'this is a test role'
-                            type = 'this is a test job type'
-                        />
-                                                <BtnCardReveal 
-                            title = 'TEST JOB TITLE BLAH BLAH'
-                            description = 'This is a test job description'
-                            role = 'this is a test role'
-                            type = 'this is a test job type'
-                        />
-                    </body>
-                </main>
-                <footer>
-                    <Footer />
-                </footer>
-            
+
+            <header>
+                <Navbar title="Dashboard">
+
+                </Navbar>
+            </header>
+            <main>
+                <body>
+                    <UserDashboardCard
+                        name="Rambo"
+                        email="Rambo@rambo.com"
+                        jobTitle="Test"
+                    />
+                    <BtnCardReveal
+                        title='TEST JOB TITLE BLAH BLAH'
+                        description='This is a test job description'
+                        role='this is a test role'
+                        type='this is a test job type'
+                    />
+                    <BtnCardReveal
+                        title='TEST JOB TITLE BLAH BLAH'
+                        description='This is a test job description'
+                        role='this is a test role'
+                        type='this is a test job type'
+                    />
+                    <BtnCardReveal
+                        title='TEST JOB TITLE BLAH BLAH'
+                        description='This is a test job description'
+                        role='this is a test role'
+                        type='this is a test job type'
+                    />
+                    <BtnCardReveal
+                        title='TEST JOB TITLE BLAH BLAH'
+                        description='This is a test job description'
+                        role='this is a test role'
+                        type='this is a test job type'
+                    />
+                </body>
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+
         </div>
     )
 }
