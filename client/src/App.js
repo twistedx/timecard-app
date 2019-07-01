ffd310bceb97af50a896500b17813f6a3238d744
abbyreyna <reyna.abby08@gmail.com>
2019-06-30 21:00:01 -0700

    conflict resolved + timecard pg rte add to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-import LoginPage from './Components/LoginPage/Login/Login';
-import Dashboard from './Components/DashboardPage/Dashboard/Dashboard';
-import React, { Fragment, useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Components/Layout/Navbar/Navbar';
-import About from './Components/pages/About';
-import Register from './Components/Register/Register';
-import AuthContext from './context/auth/AuthContext';
-=======
 import LoginPage from './components/LoginPage/Login/Login';
 import Dashboard from './components/DashboardPage/Dashboard/Dashboard';
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import About from './components/pages/About';
 import Register from './components/Register/Register';
->>>>>>> 32cc4cad29dfcce3475fec3b716aebfd0725190c
 import AuthState from './context/auth/AuthState';
 import setAuthToken from './utils/setAuthToken';
-import TimecardPage from './Components/Timecard/TimecardPage';
+import TimecardPage from './components/Timecard/TimecardPage';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Footer from './components/Layout/Footer/Footer';
@@ -36,22 +25,6 @@ const App = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
-    // <AuthState>
-    <Router>
-      <Fragment>
-        <Switch>
-          <Route exact path='/' component={LoginPage} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={LoginPage} />
-          <Route exact path='/dash' component={Dashboard} />
-          <Route exact path='/timecards' component={TimecardPage} />
-        </Switch>
-      </Fragment>
-    </Router>
-    // </AuthState>
-=======
     <AuthState>
       <Router>
         <Navbar title="Timecard App" />
@@ -61,12 +34,12 @@ const App = () => {
             <Route exact path='/about' component={About} key={About.name} />
             <Route exact path='/register' component={Register} key={Register.name} />
             <Route exact path='/login' component={LoginPage} key={LoginPage.name} />
+            <Route exact path='/timecards' component={TimecardPage} key={TimecardPage.name} />
           </Switch>
         </body>
       </Router>
       <Footer />
     </AuthState>
->>>>>>> 32cc4cad29dfcce3475fec3b716aebfd0725190c
   )
 }
 
--- client/src/components/Timecard/TimecardPage.js
+++ client/src/components/Timecard/TimecardPage.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import TcTitle from './TcTitle';
 import CreateNewBtn from './CreateNewBtn';
-import Navbar from '../Layout/Navbar/Navbar';
 import Footer from '../Layout/Footer/Footer';
 import TcList from './TcList';
 
 const TimecardPage = () => {
   return [
     <div>
-      <header>
-        <Navbar title="Timecard App" />
-      </header>
       <main>
         <body>
           <TcTitle />
