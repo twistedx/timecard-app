5838c27f916b309ff759f7c32d76be99f4f88bcc
abbyreyna <reyna.abby08@gmail.com>
2019-06-30 20:18:11 -0700

    tweaking style of timecard page

--- client/src/components/Timecard/TcList.js
+++ client/src/components/Timecard/TcList.js
@@ -1,29 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 
 const TcList = props => {
   const [cardHeight, setCardHeight] = useState();
 
+
   return (
     <div>
-      <div className="container">
-        <div className="card" style={{ height: cardHeight }}>
-          <div className="card-content">
-            <span
-              className="card-title activator grey-text text-darken-4"
-              onClick={() => setCardHeight('300px')}
-            >
-              {' '}
-              {props.title} <i className="material-icons">more_vert</i>
-            </span>
-          </div>
-          <div className="card-reveal">
-            <span
-              className="card-title grey-text text-darken-4"
-              onClick={() => setCardHeight()}
-            >
-              {' '}
-              {props.title} <i className="material-icons">close</i>
-            </span>
+      <div className="card" style = {{ height: cardHeight }}>
+        <div className="card-content">
+          <span className="card-title activator grey-text text-darken-4" onClick = { () => setCardHeight('300px') } >
+            Timecard<i class="material-icons right">more_vert</i>
+          </span>
+          <p>
+            {/* optional */}
+            <a href="#">Edit Timecard</a>
+          </p>
+        </div>
+        <div className="card-reveal">
+          <span className="card-title grey-text text-darken-4" onClick = { () => setCardHeight() }>
+            Timecard<i class="material-icons right">close</i>
+          </span>
+          <p>
             <div>
               <span className="title">Job Description: </span>
               {props.description}
@@ -36,15 +33,25 @@ const TcList = props => {
               <span className="title">Job Type: </span>
               {props.type}
             </div>
-            <div>
-              <ul>
-                <li>Clock in:</li>
-                <li>Break In:</li>
-                <li>Break Out:</li>
-                <li>Lunch In:</li>
-                <li>Lunch Out:</li>
-                <li>Clock Out:</li>
-              </ul>
+          </p>
+          <div className="collection">
+            <div className="collection-item">
+              <span class="badge">1212</span>Clock In:
+            </div>
+            <div className="collection-item">
+              <span class="badge">1133</span>Break Out:
+            </div>
+            <div className="collection-item">
+              <span class="badge">1132</span>Break In:
+            </div>
+            <div className="collection-item">
+              <span class="badge">1232</span>Lunch Out:
+            </div>
+            <div className="collection-item">
+              <span class="badge">2321</span>Lunch In:
+            </div>
+            <div className="collection-item">
+              <span class="badge">2321</span>Clock Out:
             </div>
           </div>
         </div>
