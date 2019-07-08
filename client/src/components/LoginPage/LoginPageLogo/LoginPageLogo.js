9a0e1ef69174d9ef35e1fb275b2dba83a51aed79
bufu2o2 <bufu2o2@gmail.com>
2019-07-08 15:43:46 -0700

    pushing with no bugs

--- client/src/components/LoginPage/LoginPageLogo/LoginPageLogo.js
+++ client/src/components/LoginPage/LoginPageLogo/LoginPageLogo.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './LoginPageLogo.css';
+import logo from '../../../img/timecardicon.png';
+import tag from '../../../img/TitlePage.png';
 
 const LoginPageLogo = () => {
     return (
         <div className="container">
             <div className="row">
                 <div className="col s12 m12 l12 center">
-                <img className="responsive-img PlaceholderImg" src="https://files.slack.com/files-pri/TEPHK3V9N-FKSRRMM41/titlepage.png" alt=''></img>
+                <img className="responsive-img PlaceholderImg" src = {logo} alt='' />
                 </div>
             </div>
         </div>
